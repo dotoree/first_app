62196927feab299b5b5cc13a6948a495c24e7207
George Giannoulopoulos <dotoree@640822.no-reply.drupal.org>
2013-06-26 00:36:33 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '35f10bd39cdf3e966eaa92aaf222b131abcedb1c6ea5bd5b9a1655b05ccdf51574107d6f480ec7c1779f0013376928ecfebd6ba8c5b31a505bed26e32b385970'
